63d268cc5c7c77c243fa712524410ca7b718da96
HarshidaCP <cpharshida@gmail.com>
2024-09-03 10:50:41 +0530

    Add files via upload

---
+++ harshida/basic.js
@@ -0,0 +1,8 @@
+
+let t = 18
+if (t < 20)
+    console.log("cold day")
+else if (t > 20 && t < 30)
+    console.log("normal day")
+else 
+    console.log("hot day")
---
+++ harshida/fruits.js
@@ -0,0 +1,6 @@
+let fruits = ['apple', 'banana', 'orange'];
+console.log(fruits);
+console.log(fruits[1]);
+fruits.push('kiwi')
+console.log(fruits);
+
